0892842655ddf18377b91b01a77c8061dde0f445
Tomáš Hubálek <tom.hubalek@gmail.com>
2023-07-20 09:31:10 +0200

    Added tina cms

---
+++ tina/config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "ff6d0204-13fe-4537-adef-52045892f8da", // Get this from tina.io
+  token: "140027ff485a9c71d50bb32b33ccbe8dec66ac10", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "./",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "./",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
